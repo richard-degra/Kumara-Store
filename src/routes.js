167292bed338d5c8019a777613b8e311a7cd7a4d
richard-degra <rich_deg@outlook.com>
2020-07-22 16:09:45 -0300

    rotas

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const routes = express.Router()
+const ProductController = require('./app/controllers/productController')
 
 routes.get('/', function (req, res) {
     return res.render('layout.njk')
 })
 
-// Rotas dos Instrutores //
+routes.get('/products/create', ProductController.create)
 
+routes.get('/ads/create', function (req, res) {
+    return res.redirect('/products/create')
+})
 
 module.exports = routes
